3cf396d36538245d7148cb0fcb514f780877fb39
P Ajay Rao <ajayraop3495@gmail.com>
2017-02-19 16:34:20 +0530

    Changes to create_grapgh and get  graph return value

--- src/graph.py
+++ src/graph.py
@@ -10,13 +10,13 @@ class Graph:
 
     def _get_weights(self, shape, name='weights'):
         with tf.name_scope(name):
-            weights = tf.Variable(tf.truncated_normal(shape, stddev=0.1))
+            weights = tf.Variable(tf.truncated_normal(shape, stddev=0.05))
         tf.summary.histogram(name, weights)
         return weights
 
     def _get_bias(self, shape, name='bias'):
         with tf.name_scope(name):
-            bias = tf.Variable(tf.constant(0.1, shape=shape))
+            bias = tf.Variable(tf.constant(0.05, shape=shape))
         tf.summary.histogram(name, bias)
         return bias
 
@@ -29,68 +29,78 @@ class Graph:
             return tf.nn.max_pool(input_layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     def _create_new_graph(self):
-        with tf.name_scope("Input"):
-            input_layer = tf.placeholder(tf.float32, shape=[None, 25600],name='id')
-            input = tf.reshape(input_layer, [-1, 80, 320, 1])
-        '''with tf.name_scope("Output"):
-            action_value = tf.placeholder(tf.float32, shape=[None, self.actions])'''
 
-        with tf.name_scope("Layer1"):
+        with tf.name_scope('Input'):
+            input_layer = tf.placeholder(tf.float32, shape=[None, 25600], name='id')
+            input = tf.reshape(input_layer, [-1, 80, 320, 1])
+
+        with tf.name_scope('Updated_value'):
+            updated_action = tf.placeholder(tf.float32, shape=[None, self.actions])
+
+        with tf.name_scope('Layer1'):
             conv_1_weights = self._get_weights([5, 5, 1, 32])
             conv_1_bias = self._get_bias([32])
             conv_layer_1 = tf.nn.relu(self._convolution_layer(input, conv_1_weights) + conv_1_bias)
             conv_pool_1 = self._max_pooling_layer(conv_layer_1)
 
-        with tf.name_scope("Layer2"):
+        with tf.name_scope('Layer2'):
             conv_2_weights = self._get_weights([5, 5, 32, 64])
             conv_2_bias = self._get_bias([64])
             conv_layer_2 = tf.nn.relu(self._convolution_layer(conv_pool_1, conv_2_weights) + conv_2_bias)
             conv_pool_2 = self._max_pooling_layer(conv_layer_2)
 
-        with tf.name_scope("Layer3"):
+        with tf.name_scope('Layer3'):
             conv_3_weights = self._get_weights([5, 5, 64, 128])
             conv_3_bias = self._get_bias([128])
             conv_layer_3 = tf.nn.relu(self._convolution_layer(conv_pool_2, conv_3_weights) + conv_3_bias)
             conv_pool_3 = self._max_pooling_layer(conv_layer_3)
 
-        with tf.name_scope("Layer4"):
+        with tf.name_scope('Layer4'):
             conv_4_weights = self._get_weights([5, 5, 128, 256])
             conv_4_bias = self._get_bias([256])
             conv_layer_4 = tf.nn.relu(self._convolution_layer(conv_pool_3, conv_4_weights) + conv_4_bias)
             conv_pool_4 = self._max_pooling_layer(conv_layer_4)
 
-        with tf.name_scope("HiddenLayer"):
+        with tf.name_scope('HiddenLayer'):
             hidden_1_weights = self._get_weights([5 * 20 * 256, 2048])
             hidden_1_bias = self._get_bias([2048])
             hidden_layer_input = tf.reshape(conv_pool_4, [-1, 5 * 20 * 256])
             hidden_1_layer_output = tf.nn.relu(tf.matmul(hidden_layer_input, hidden_1_weights) + hidden_1_bias)
 
-        with tf.name_scope("ReadoutLayer"):
+        with tf.name_scope('ReadoutLayer'):
             readout_weights = self._get_weights([2048, self.actions])
             readout_bias = self._get_bias([self.actions])
 
-            action = tf.matmul(hidden_1_layer_output, readout_weights) + readout_bias
-        '''
-        with tf.name_scope("Loss"):
-            loss = action_value - action
-            tf.summary.histogram("Loss", loss)
-        final_graph = tf.train.AdamOptimizer(1e-4).minimize(loss)
-        '''
+        with tf.name_scope('Output_Distribution'):
+            action_taken = tf.matmul(hidden_1_layer_output, readout_weights) + readout_bias
+            tf.summary.histogram('output',action_taken)
 
-        return action, input_layer
+        with tf.name_scope("Loss"):
+            loss = updated_action-action_taken
+            tf.summary.histogram("Loss", loss)
+
+        train_op = tf.train.AdamOptimizer(1e-4).minimize(loss)
+
+
+        return action_taken, input_layer, loss,updated_action ,train_op
+
+    def _load_graph(self, load_dir):
+        saver = tf.train.Saver()
+        sess = tf.Session()
+        saver.restore(sess, self.load_dir)
+        tf.logging.INFO('Successfully loaded the graph')
+
+    def save_graph(self,save_dir):
+        saver = tf.train.Saver()
+        sess = tf.Session()
+        saver.save(sess, save_dir)
+        tf.logging.INFO('Successfully saved the graph at %s'%save_dir)
 
     def get_graph(self):
-        self.graph, self.input_layer = self._create_new_graph()
+        self.graph , input_layer ,loss,updated_action,train_op = self._create_new_graph()
         if self.load_dir:
-            saver = tf.train.Saver()
-            sess = tf.Session()
-            saver.restore(sess, self.load_dir)
-            tf.logging.INFO('Successfully loaded the graph')
-        return self.graph
-
-    def run_graph(self, session, image):
-        print(session.run(self.graph, feed_dict={self.input_layer: image}))
-
+            self._load_graph(self.load_dir)
+        return self.graph, input_layer, loss , train_op
 
 if __name__ == '__main__':
     Graph(action=10).get_graph()
