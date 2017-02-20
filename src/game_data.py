43621f4c2b66e43affc9bf2d75291dadc7663607
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2017-02-20 16:16:42 +0530

    * Join `Executor` with `GameBatchData` * Write `GameBatchData` to `CSV`

--- src/game_data.py
+++ src/game_data.py
@@ -1,9 +1,17 @@
+from src.executor import Executor
+
+
 class GameBatchData:
-    def __init__(self):
+    def __init__(self, timestamp):
+        self.execution_timestamp = timestamp
         self.data = []
         self.game_timestamps = []
         self.current_game_index = None
         self.steps = 0
+        self.game_data_headers = [
+            'index', 'timestamp', 'reward',
+            'action_value', 'action', 'loss'
+        ]
 
     def new_game(self, timestamp):
         self.game_timestamps.append(timestamp)
@@ -31,6 +39,25 @@ class GameBatchData:
 
     def get_data(self):
         return self.data
+
+    def save_progress(self):
+        executor = Executor(execution_path="summary", execution_timestamp=self.execution_timestamp)
+        for i, game in enumerate(self.data):
+            game_timestamp = self.game_timestamps[i]
+            executor.new_game(game_timestamp)
+
+            # Save screenshots
+            screenshots = []
+            concatenated_screenshots = []
+            for step in game:
+                screenshots.append((step['observation'], step['timestamp']))
+                concatenated_screenshots.append((step['concatenated_observation'], step['timestamp']))
+            executor.add_screenshots(screenshots)
+            executor.add_screenshots(concatenated_screenshots, 'concat_screenshots')
+
+            # Write data to CSV
+            executor.write_csv(headers=self.game_data_headers,
+                               data=game, game_timestamp=game_timestamp)
 
     def get_total_steps(self):
         return self.steps
